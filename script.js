2764c5d2f2abda72a79f1a478c35342e7138e401
sleserg <sleserg@gmail.com>
2025-07-09 14:27:28 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function setLightTheme() {
+   document.body.style.color = 'black';
+   document.body.style.backgroundColor = 'white';
+}
+
+function setDarkTheme() {
+   document.body.style.color = 'white';
+   document.body.style.backgroundColor = 'darkgrey';
+}
